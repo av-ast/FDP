54e1d2db110328999bf06badf45c7a6a37763934
Alexey Astafyev <av.astafyev@gmail.com>
2013-02-07 11:44:13 +0400

    test running thread

--- test/lib/feature_test.rb
+++ test/lib/feature_test.rb
@@ -2,8 +2,13 @@ require 'test_helper'
 
 class FeatureTest < TestCase
 
-  def test_should_assert
-    future = future { 2 + 2 }
-    assert_equal 4, future.value
+  def test_simple_assert
+    f = future { 2 + 2 }
+    assert_equal 4, f.value
+  end
+
+  def test_inspect_running_thread
+    f = future { sleep 1; 2 + 2 }
+    assert_equal "#<Future running>", f.inspect
   end
 end
